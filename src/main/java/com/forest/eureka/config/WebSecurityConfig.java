9cf9039e52ac03298b0b73b19f5be9a378796da4
zhangsonglin <zhangsonglin@quyiyuan.com>
2022-06-13 10:50:14 +0800

    最简配的Eureka注册中心

---
+++ src/main/java/com/forest/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.forest.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and().authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
+    }
+}
